88cfcf991065ef2106a48ae5b0402ad2a7ee1cfc
Dmitry Scherbakov <dima.scherbie@gmail.com>
2023-04-07 22:00:06 +0000

    az vault - working with secrets

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 namespace dotnetcore
 {
@@ -6,7 +9,40 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string secretName = "dimazaurus";
+            string keyVaultName = "zarium-vault";
+            var kvUri = "https://zarium-vault.vault.azure.net/";
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+                {
+                    Delay= TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                 }
+            };
+
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential(),options);
+
+            KeyVaultSecret secret = client.GetSecret(secretName);
+
+            Console.WriteLine($"GetSecret:'{secret.Value}'");
+
+            Console.Write("Enter Secret > ");
+            string secretValue = Console.ReadLine();
+            client.SetSecret(secretName, secretValue);
+            Console.Write("SetSecret:");
+            Console.Write("  Key: " + secretName);
+            Console.WriteLine(" Value: " + secretValue);
+
+            Console.WriteLine($"GetSecret:'{secret.Value}'");
+
+            client.StartDeleteSecret(secretName);
+            Console.WriteLine($"StartDeleteSecret from key vault: {keyVaultName}");
+
+            Console.WriteLine($"GetSecret:'{secret.Value}");
+
         }
     }
 }
---
+++ terminal-history.md
@@ -0,0 +1,15 @@
+gitpod /workspace/zarium-vault (master) $ history
+    1  dotnet run
+    2  dotnet add package Azure.Identity 
+    3  dotnet add package Azure.Security.KeyVault.Secrets
+    4  dotnet build
+    5  dotnet run
+    6  az login
+    7  cls
+    8  clear
+    9  curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+   10  az login --use-device-code
+   11  dotnet build
+   12  dotnet run
+   13  clear
+   14  history
